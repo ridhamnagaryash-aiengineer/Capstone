a744e76ebd38388804561d2d49c5c8d69c70b5e2
Shivam Katiyar <shivamkti45@gmail.com>
2025-11-17 12:32:35 +0530

    allow origin

--- main.py
+++ main.py
@@ -28,7 +28,11 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust in production
+    allow_origins=[
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
+    ],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
