f008c693f0f9b1640719030e23d82bfef3007a6c
Shivam Katiyar <shivamkti45@gmail.com>
2025-11-17 12:33:13 +0530

    allow origin  agein

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "http://localhost:8080",
+        "http://127.0.0.1:8080"
     ],
 
     allow_credentials=True,
